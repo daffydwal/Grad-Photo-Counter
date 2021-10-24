208aa47c21e9c17b66533c75e93ff84d029f30ad
David Wale <daffydwal@googlemail.com>
2021-10-24 01:13:48 +0100

    Can now select CSV, add to local storage, and show from storage as list on page

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-//const localforage = require("localforage");
-
-//console.log('Localforage is: ', localforage)
-
-const theForm = document.querySelector('#theForm');
+const uploadForm = document.querySelector('#uploadForm');
 const csvFile = document.querySelector('#csvFile');
+const fullList = localforage.createInstance({name: 'FullList'});
+const cereName = document.querySelector('#cereName');
+const cereList = document.querySelector('#ceremoniesList');
+const cereSelectForm = document.querySelector('#ceremonySelector');
+
+populateCeremoniesList();
 
 let studentList = [];
 
-theForm.addEventListener('submit', function (e){
+// event listeners
+
+uploadForm.addEventListener('submit', function (e){
     e.preventDefault();
     const input = csvFile.files[0];
     const reader = new FileReader();
@@ -15,14 +19,56 @@ theForm.addEventListener('submit', function (e){
     reader.onload = function (e) {
         const text = e.target.result;
         studentList = csvToArray(text);
-        studentsToList(studentList);
+        //studentsToList(studentList);
+        listCeremonyDB(cereName.value);
     };
-
     reader.readAsText(input);
+})
+
+cereSelectForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const selected = cereList.value;
+
+    const db = localforage.createInstance({name: selected});
+    const ListBox = document.querySelector('#list')
+    const list = document.createElement('ol');
+    ListBox.textContent = '';
+    db.iterate(function (value, key, i){
+        const item = document.createElement('li');
+        item.textContent = 'Name: ' + value.Name + ',   Student Number: ' + value.StudNum;
+        list.appendChild(item);
+    })
+
+    ListBox.appendChild(list);
+    
 })
 
 
 //functions be here
+
+function createCeremonyDB(dbName){
+    const db = localforage.createInstance({name: dbName});
+    let ittNum = "1000";
+    studentList.forEach(item =>{
+        db.setItem(ittNum, {StudNum: item.StudNum, Name: item.Name}).then(function (){});
+        ittNum++
+        ittNum = ittNum.toString();
+    })
+
+}
+
+function listCeremonyDB(dbName){
+    fullList.getItem(dbName, function (err, value){
+        if (value !== null){
+            alert(value + ' already exists!');
+            return;
+        }else{
+            fullList.setItem(dbName, dbName)
+            createCeremonyDB(dbName);
+        }
+    })
+}
 
 function csvToArray(str, delimiter = ","){
     const headers = str.slice(0, str.indexOf("\r")).split(delimiter);
@@ -51,4 +97,13 @@ function studentsToList (array){
     });
 
     ListBox.appendChild(list);
+}
+
+function populateCeremoniesList(){
+    fullList.iterate(function(value, key, iterationNumber){
+        item = document.createElement('option');
+        item.value = value;
+        item.textContent = value;
+        cereList.appendChild(item);
+    })
 }
