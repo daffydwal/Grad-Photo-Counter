99f782380d2399085bed63000d1fb190dac32b76
David Wale <daffydwal@googlemail.com>
2021-10-27 11:34:21 +0100

    Added watermarking to script

--- python_script/rename_photos.py
+++ python_script/rename_photos.py
@@ -2,22 +2,34 @@ import os
 import csv
 
 directory = input("Please name this ceremony: ")
-thumbDir = directory + "/Thumbnails"
+sampDir = directory + "/Samples"
 os.mkdir(directory)
-os.mkdir(thumbDir)
+os.mkdir(sampDir)
 
 with open('Photos_List.csv') as f:
     lines = csv.DictReader(f)
+
+    # totalRows = 0
+    # for row in lines:
+    #     totalRows += 1
+    #     print('Added a row')
+    
+    # print('Moving on')
+
     for idx, line in enumerate(lines):
         try:
             newName = './' + directory + '/' + line['PhotoNum'] + ' - ' + line['StudNum'] + ' - ' + line['Name'] +  '.JPG'
-            newThumb = './' + directory + '/Thumbnails/' + line['PhotoNum'] + ' - ' + line['StudNum'] + ' - ' + line['Name'] + '.JPG'
-            compComm = 'magick ' + line['PhotoNum'] + '.JPG -resize 320 -quality 70 ' + '"' +  newThumb + '"'
+            newSamp = './' + directory + '/Samples/' + line['PhotoNum'] + ' - ' + line['StudNum'] + ' - ' + line['Name'] + '.JPG'
+            compComm = 'magick ' + line['PhotoNum'] + '.JPG -resize 800 -quality 70 compressed.JPG'
+            sampComm = 'magick compressed.JPG WATERMARK.png -composite -gravity center ' + '"' + newSamp + '"'
+            print('Processing photo ' + str(idx+1))
             os.system(compComm)
+            os.system(sampComm)
             os.rename(line['PhotoNum'] + '.JPG', newName)
-            
+            os.remove('compressed.JPG')
         except FileNotFoundError:
             print('File not found!')
             pass
 
-os.rename('Photos_List.csv', './' + directory + '/Processed_Photos.csv')
+# os.rename('Photos_List.csv', './' + directory + '/Processed_Photos.csv')
+
